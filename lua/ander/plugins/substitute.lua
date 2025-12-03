a981e77068ba70d157538297be5a25537b962f27
anderdc <me@alexanderdc.com>
2025-12-02 21:29:48 -0600

    add yazi, update telescope

--- lua/ander/plugins/substitute.lua
+++ lua/ander/plugins/substitute.lua
@@ -9,9 +9,9 @@ return {
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>r", substitute.operator, { desc = "Substitute with motion" })
-    keymap.set("n", "<leader>rr", substitute.line, { desc = "Substitute line" })
-    keymap.set("n", "<leader>R", substitute.eol, { desc = "Substitute to end of line" })
-    keymap.set("x", "<leader>r", substitute.visual, { desc = "Substitute in visual mode" })
+    keymap.set("n", "<leader>s", substitute.operator, { desc = "Substitute with motion" })
+    keymap.set("n", "<leader>ss", substitute.line, { desc = "Substitute line" })
+    keymap.set("n", "<leader>S", substitute.eol, { desc = "Substitute to end of line" })
+    keymap.set("x", "<leader>s", substitute.visual, { desc = "Substitute in visual mode" })
   end,
 }
