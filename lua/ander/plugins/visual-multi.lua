ed19c87d8acbe49ef0976caf3d797bc853843e66
anderdc <alexanderdcastaneda@gmail.com>
2025-12-13 16:41:24 -0600

    pr-reviewer and multi line plugins

---
+++ lua/ander/plugins/visual-multi.lua
@@ -0,0 +1,12 @@
+return {
+	"mg979/vim-visual-multi",
+	branch = "master", -- optional: recommend explicitly if you want master branch
+	keys = {
+		{ "<C-n>" },
+		{ "gV" },
+	},
+	config = function()
+		-- nothing to configure by default —
+		-- plugin works out of the box
+	end,
+}
