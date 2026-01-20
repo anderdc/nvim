e090afe1f4dfcdcb358c97f77dcf200762b6c605
anderdc <alexanderdcastaneda@gmail.com>
2026-01-20 09:56:16 -0600

    format all plugins

--- lua/ander/plugins/vim-maximizer.lua
+++ lua/ander/plugins/vim-maximizer.lua
@@ -1,6 +1,6 @@
 return {
-  "szw/vim-maximizer",
-  keys = {
-    {"<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Mazimize/minimize a split"},
-},
+	"szw/vim-maximizer",
+	keys = {
+		{ "<leader>sm", "<cmd>MaximizerToggle<CR>", desc = "Mazimize/minimize a split" },
+	},
 }
